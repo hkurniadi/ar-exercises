92eb2f479cdc3b69cac65673cde274c964ee14ba
Hans Kurniadi <hans.d.k@live.com>
2017-03-14 19:08:05 +0000

    ex2, load store to instance variables using find method, and update using update method

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+pp @store1
+
+@store2 = Store.find(2)
+pp @store2
+
+@store1.update({
+  name: "Coquitlam",
+  updated_at: Time.now
+  })
+pp @store1
