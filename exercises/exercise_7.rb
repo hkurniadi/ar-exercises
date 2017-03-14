67ab073a14abcb79be2fbf61c87de51b7ebbfec0
Hans Kurniadi <hans.d.k@live.com>
2017-03-14 21:19:52 +0000

    ex7, define validations on Employee and Store classes

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,19 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Ask for a store name from user
+@user_input = gets.chomp
+# Create the store instance using just name
+@user_store = Store.create({name: @user_input})
+puts @user_store.errors.messages
+# {:annual_revenue=>["is not a number"], :mens_apparel=>["Has to be a men's or women's appare, store"]}
+
+# Create the store instance using name and annual revenue
+@user_store = Store.create({name: @user_input, annual_revenue: 400000})
+puts @user_store.errors.messages
+# {:mens_apparel=>["Has to be a men's or women's appare, store"]}
+
+# Create the store instance using name, annual revenue, and men's apparel
+@user_store = Store.create({name: @user_input, annual_revenue: 400000, mens_apparel: true})
+puts @user_store.errors.messages
+# {}
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true }
+  validate :mens_or_womens_apparel
+
+  def mens_or_womens_apparel
+    if !mens_apparel && !womens_apparel
+        errors.add(:mens_apparel, "Has to be a men's or women's appare, store")
+    end
+  end
 end
