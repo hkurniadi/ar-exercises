e85fc18db04b41858fc35850d816557e469f5759
Hans Kurniadi <hans.d.k@live.com>
2017-03-14 19:38:17 +0000

    ex5, use chaining methods and calculations method

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenues = Store.sum("annual_revenue")
+puts "Total revenues from all stores #{@total_revenues}"
+
+@average_annual = Store.average("annual_revenue")
+puts "Average revenues from all stores #{@average_annual}"
+
+@number_of_high_sales_stores = Store.where("annual_revenue > 1000000").count
+
+puts "There are #{@number_of_high_sales_stores} stores that generate more than $1M"
